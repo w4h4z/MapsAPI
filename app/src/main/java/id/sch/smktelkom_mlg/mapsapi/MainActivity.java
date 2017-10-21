1ed216806b8cfcf3cad06f3800260a425a238f13
rizaldi_wahaz <rizaldi.wahaz@gmail.com>
2017-10-21 09:14:17 +0700

    Add Maps

--- app/src/main/java/id/sch/smktelkom_mlg/mapsapi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/mapsapi/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.mapsapi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import id.sch.smktelkom_mlg.gmaps.R;
 
 public class MainActivity extends AppCompatActivity {
 
